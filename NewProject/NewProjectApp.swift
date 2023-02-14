01da3c8b883d7b51b91468f958400178c2296db6
mandooplz <kmw4561@gmail.com>
2023-02-14 15:27:16 +0900

    2023.02.14 NewStart

---
+++ NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by 김민우 on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
